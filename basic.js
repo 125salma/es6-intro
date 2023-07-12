8e5765a41e652de7e358bfeeb570668b01df6624
winmiraj@gmail.com <winmiraj@gmail.com>
2023-07-12 07:49:24 +0600

    first commit

---
+++ basic.js
@@ -0,0 +1,56 @@
+//variable
+var deposit=400;
+//conditon
+if(deposit>500){
+    
+}
+else if(deposit<200){
+
+}
+else if(deposit==500){
+
+}
+else if(deposit!=500){
+
+}
+else if(deposit>=500){
+
+}
+else if(deposit<=500){
+
+}
+else{
+
+}
+//array
+const numbers = [56,50,42,30,444];
+const numberCount = numbers.length;//number er modde koyta element ase
+numbers.pop();//kono kono number remove korte chaile
+numbers.push(111);//new number add korte gele
+numbers[2]=555;//chaile 2 index er man change kora jay
+
+//check weather 222 included in the array
+if(numbers.indexOf(222) !=-1){ //array number find out .no find out will be -1
+
+}
+else if(numbers.includes(222) !=-1){//thakle true,na thakle false
+
+}
+
+//loop
+//for,while
+for(const number of numbers){
+
+}
+//function
+function fullName(first,second){
+    const name= first+ ' '+second
+}
+fullName('Rahim','Ali');
+
+//object
+const bottle = {
+    color:'yellow',
+    contain:'water',
+    price:16666
+}
